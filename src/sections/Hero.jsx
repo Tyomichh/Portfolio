61008bcbff2272ad5be45fa7c48d39dc28a43d96
Tyomichh <workemailaertemlozenko@gmail.com>
2025-09-12 14:02:35 +0300

    Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,6 +3,7 @@
 // 2. Контекст і хуки
 // 3. Компоненти
 // 4. Іконки та зображення
+import MyPhoto from "../assets/MyPhoto.jpg";
 // 5. Стилі
 import "./Hero.scss"
 // 6. Дані
@@ -10,8 +11,22 @@ import "./Hero.scss"
 function Hero() {
 
   return (
-    <section>
-      <h1 className="hero_position-name">Frontend Developer</h1>
+    <section className="hero">
+      <div className="hero_position">
+        <h1 className="hero_position-name">Developer <br /> Frontend</h1>
+        <img src={MyPhoto} alt="Artem Lozenko" className="hero_photo" />
+      </div>
+
+      <div className="hero_about">
+        <h1 className="hero_name">Artem <br />Lozenko</h1>
+        <p className="hero_about-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <a href="/resume.pdf" download className="hero_resume-btn">
+          Resume CV
+        </a>
+      </div>
     </section>
   )
 }
